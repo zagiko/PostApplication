b118d8bc8207c6a651b5d0ac8d48f5e4c41c32c0
zagiko <m.sagik@gmail.com>
2023-09-05 17:28:26 +0300

    Initial Commit

---
+++ PostApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostApplication
+//
+//  Created by Mykhailo Zagiko on 05.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
